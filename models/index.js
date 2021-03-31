0050fefb9212f18ba4d81a5f28a2999a79b2c52d
Theresa Pidutti <treepidutti@gmail.com>
2021-03-31 10:55:56 -0600

    update models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: '',
+});
 // Categories have many Products
-
+Category.hasMany(Products, {
+  foreignKey: '',
+  onDelete: '',
+});
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
 
+});
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+
+});
 
 module.exports = {
   Product,
