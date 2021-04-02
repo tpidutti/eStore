32d6e0939d3c9166fc13f2c3f5a853a45e795828
Theresa Pidutti <treepidutti@gmail.com>
2021-04-02 07:15:52 -0600

    update routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-// const sequelize = require('../config/connection');
+
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
@@ -34,12 +34,22 @@ router.get('/:id', (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  
   const tagData = await Tag.create(req.body);
 
   return res.json(tagData);
 });
+
+// router.post('/', async (req, res) => {
+//   try {
+//     const tagData = await Tag.create(req.body);
+//     res.status(200).json(tagData);
+//   } catch (err) {
+//     res.status(400).json(err);
+//   }
+// });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
@@ -56,14 +66,23 @@ router.put('/:id', (req, res) => {
   );
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
-  const tagData = await Tag.destroy({
-    where: {
-      tag_id: req.params.tag_id,
-    },
-  });
-  return res.json(tagData);
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        tag_id: req.params.tag_id,
+      },
+    });
+if (!tagData) {
+  res.status(404).json({
+    message: 'No tag with that id found.'});
+    return;
+}
+res.status(200).json(tagData);
+  }catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
